5f4531b01a2b220073a92a8d872cf8ff4f14ee51
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-07-26 17:38:01 -0300

    Ajustes no layout

--- src/screen/Agendar.js
+++ src/screen/Agendar.js
@@ -1,16 +1,41 @@
-import { View, Text, Pressable } from "react-native";
+import { View, StyleSheet, TextInput, Pressable } from "react-native";
 import React from "react";
 import SafeContainer from "../components/SafeContainer";
+
+import { Avatar, Button, Card, Text } from "react-native-paper";
+import {
+  CircleUserRound,
+  ChevronLeft,
+  UserRoundPen,
+  Trash2,
+  LogOut,
+} from "lucide-react-native";
 
 export default function Agendar({ navigation }) {
   return (
     <SafeContainer>
-      <View>
-        <Text>Agendamento</Text>
-        <Pressable onPress={() => navigation.navigate("Login")}>
-          <Text>Login </Text>
-        </Pressable>
+      <View style={estilos.cabecalho}>
+        <ChevronLeft color="#ffff" size={32} />
+        <Text style={estilos.textoCabecalho}> AGENDAMENTOS</Text>
+        <Avatar.Text size={39} label="GS" />
       </View>
     </SafeContainer>
   );
 }
+
+const estilos = StyleSheet.create({
+  cabecalho: {
+    backgroundColor: "#f57856",
+    width: "100%",
+    padding: 10,
+    paddingTop: 30,
+    paddingBottom: 25,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textoCabecalho: {
+    color: "#ffff",
+    fontSize: 14,
+  },
+});
