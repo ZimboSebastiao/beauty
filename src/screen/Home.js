ae159b333ebfe04cf93994c6af6dd43ff176b1b3
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-07-26 04:05:23 -0300

    update

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,31 +1,26 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
 import React from "react";
 import SafeContainer from "../components/SafeContainer";
+import Cabelo from "../../assets/images/cabelo.jpg";
 
 import { Avatar, Button, Card, Text } from "react-native-paper";
 
-const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
+// const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
 
 export default function Home({ navigation }) {
   return (
     <SafeContainer>
       <Text>Home</Text>
-      <View>
+      <View style={estilos.cartao}>
         <Card>
-          <Card.Title
-            title="Card Title"
-            subtitle="Card Subtitle"
-            left={LeftContent}
-          />
-          <Card.Content>
+          {/* <Card.Title
+          // left={LeftContent}
+          /> */}
+          <Card.Content style={estilos.cartaoFormata}>
+            {/* <Card.Cover source={{ uri: "https://picsum.photos/700" }} /> */}
+            <Image source={Cabelo} style={estilos.imagem} />
             <Text variant="titleLarge">Card title</Text>
-            <Text variant="bodyMedium">Card content</Text>
           </Card.Content>
-          <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
-          <Card.Actions>
-            <Button>Cancel</Button>
-            <Button>Ok</Button>
-          </Card.Actions>
         </Card>
       </View>
     </SafeContainer>
@@ -34,6 +29,18 @@ export default function Home({ navigation }) {
 
 const estilos = StyleSheet.create({
   cartao: {
+    width: "80%",
     backgroundColor: "red",
+  },
+  cartaoFormata: {
+    backgroundColor: "blue",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  imagem: {
+    backgroundColor: "red",
+    width: "60%",
+    // height: "100%",
   },
 });
